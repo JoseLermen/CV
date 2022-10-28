8a501d7cdb5bfa64ac9a1d17eccaa25799d4353e
José Arthur Lermen <110497031+JoseLermen@users.noreply.github.com>
2022-10-28 01:11:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -64,6 +64,16 @@ function menuCont(){
     contato.style.display = "grid";
 }
 
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
 
 const menu1 = document.getElementById("intro");
 const menu2 = document.getElementById("desc");
@@ -71,6 +81,13 @@ const menu3 = document.getElementById("edu");
 const menu4 = document.getElementById("habi");
 const menu5 = document.getElementById("exp");
 const menu6 = document.getElementById("cont");
+
+const mobile1 = document.querySelector(".intro");
+const mobile2 = document.querySelector(".desc");
+const mobile3 = document.querySelector(".edu");
+const mobile4 = document.querySelector(".habi");
+const mobile5 = document.querySelector(".exp");
+const mobile6 = document.querySelector(".cont");
 
 
 menu1.addEventListener("click", menuIntro);
@@ -84,3 +101,15 @@ menu4.addEventListener("click", menuHab);
 menu5.addEventListener("click", menuExp);
 
 menu6.addEventListener("click", menuCont);
+
+mobile1.addEventListener("click", menuIntro);
+
+mobile2.addEventListener("click", menuDesc);
+
+mobile3.addEventListener("click", menuEdu);
+
+mobile4.addEventListener("click", menuHab);
+
+mobile5.addEventListener("click", menuExp);
+
+mobile6.addEventListener("click", menuCont);
